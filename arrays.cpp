40782e2aaf2bc4cd4bf09981f32d21feba35854b
Ghazy <ahmedfathyghazy@gmail.com>
2019-04-05 01:22:07 +0200

    Solved multiply strings problem

--- arrays.cpp
+++ arrays.cpp
@@ -8,6 +8,54 @@ for_each(a.begin(), a.end(), [](const int & cur) -> void {
     cout << endl;
     }
 
+void print(string a) {
+cout << a << endl;
+    }
+
+    // https://leetcode.com/problems/multiply-strings/
+string multiply(string a, string b) {
+
+    bool neg = false;
+
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    if(a.back() == '-') neg ^= true, a.pop_back();
+    if(b.back() == '-') neg ^= true, b.pop_back();
+
+    vector<int> c(a.size() + b.size(), 0);
+
+    for(int i = 0; i < a.size(); ++i) {
+        for(int j = 0; j < b.size(); ++j) {
+            int cur = (a[i] - '0') * (b[j] - '0') + c[i + j];
+            c[i + j] = cur % 10;
+            c[i + j + 1] += cur / 10;
+        }
+    }
+
+    // check the size for input "0" & "0"
+    while(c.back() == 0 && c.size() > 1) c.pop_back();
+
+    vector<char> ret;
+
+    for_each(c.begin(), c.end(), [&ret](const int & a) {
+             ret.push_back(a + '0');
+             });
+
+    if(neg) ret.push_back('-');
+
+
+    reverse(ret.begin(), ret.end());
+     return string(ret.begin(), ret.end());
+
+}
+
+void print(vector<char> a) {
+for_each(a.begin(), a.end(), [](const char & cur) -> void {
+        cout << cur << " ";
+    });
+    cout << endl;
+    }
+
 //https://leetcode.com/problems/sort-colors
 void sortColors(vector<int>& a) {
     int n = a.size();
@@ -35,37 +83,39 @@ void addOne(vector<int> & v) {
 }
 
 void test() {
-    array<int, 5> a = {1, 2, 5, 7, 9};
+//    array<int, 5> a = {1, 2, 5, 7, 9};
+//
+//    array<int, 5> c = {1, 2, 5, 7, 9};
+////    reverse(a.begin(), a.end());
+//
+////    rotate(a.begin(), a.begin() + 3, a.end());
+//    for_each(a.begin(), a.end(), [](const int & cur) -> void {
+//        cout << cur << endl;
+//    });
+//    cout << (a == c) << endl;
+//
+//    cout << binary_search(a.begin(), a.end(), 5) << endl;
+//    cout << binary_search(a.begin(), a.end(), 4) << endl;
+//
+//    array<int, 5> b = {9, 7, 5, 3, 1};
+//    cout << binary_search(b.begin(), b.end(), 7, greater<int>()) << endl;
+//    cout << binary_search(b.begin(), b.end(), 8, greater<int>()) << endl;
+//
+//    vector<int> t = {9, 9, 9, 9};
+//
+//    addOne(t);
+//    print(t);
+//    t = {0};
+//
+//    addOne(t);
+//    print(t);
+//
+//    t = {1, 9};
+//    addOne(t);
+//    print(t);
 
-    array<int, 5> c = {1, 2, 5, 7, 9};
-//    reverse(a.begin(), a.end());
 
-//    rotate(a.begin(), a.begin() + 3, a.end());
-    for_each(a.begin(), a.end(), [](const int & cur) -> void {
-        cout << cur << endl;
-    });
-    cout << (a == c) << endl;
-
-    cout << binary_search(a.begin(), a.end(), 5) << endl;
-    cout << binary_search(a.begin(), a.end(), 4) << endl;
-
-    array<int, 5> b = {9, 7, 5, 3, 1};
-    cout << binary_search(b.begin(), b.end(), 7, greater<int>()) << endl;
-    cout << binary_search(b.begin(), b.end(), 8, greater<int>()) << endl;
-
-    vector<int> t = {9, 9, 9, 9};
-
-    addOne(t);
-    print(t);
-    t = {0};
-
-    addOne(t);
-    print(t);
-
-    t = {1, 9};
-    addOne(t);
-    print(t);
-
+    print(multiply({'-', '7', '6', '1', '8', '3', '8', '2', '5', '7', '2', '8', '7'}, {'1', '9', '3', '7', '0', '7', '7', '2', '1'}));
 }
 
 int main() {
