8476a07110914888f49d3057277b6171b5f94032
Ghazy <ahmedfathyghazy@gmail.com>
2019-05-07 18:30:06 -0500

    https://leetcode.com/problems/edit-distance/submissions/

--- dp.cpp
+++ dp.cpp
@@ -89,6 +89,36 @@ int coinChange(vector<int>& coins, int amount) {
     return sol == numeric_limits<int>::max() ? -1 : sol;
     }
 
+int minDistance(string word1, string word2) {
+int n = word1.size();
+        int m = word2.size();
+
+        vector<vector<int>> dp(2, vector<int>(m + 1, 0));
+
+        
+
+        for(int i = 1; i <= m; ++i) dp[0][i] = i;
+
+
+        for(int i = 1; i <= n; ++i) {
+            fill(dp[i%2].begin(), dp[i % 2].end(), 0);
+            dp[i % 2][0] = i;
+            for(int j = 1; j <= m; ++j) {
+
+                int & ret = dp[i % 2][j];
+
+                if(word1[i - 1] == word2[j - 1]){ ret = dp[(i - 1) % 2][j - 1];
+                } else {
+                    ret = 
+                        1 + min(dp[(i - 1) % 2][j - 1], min(dp[(i - 1) % 2][j], dp[i % 2][j - 1]));
+                }
+
+            }
+        }
+
+        return dp[n % 2][m];
+}
+
 void test() {
     cout << fib(1) << endl;
     cout << fib(8) << endl;
