a84382909f8524afaf2409d00bfd2190a2ab5496
Ghazy <ahmedfathyghazy@gmail.com>
2019-06-02 11:20:46 +0200

    is valid BST optimal solution

--- tree.cpp
+++ tree.cpp
@@ -42,6 +42,35 @@ public:
     }
 };
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+class SolutionOptimalIsValidBST {
+public:
+    
+    bool isValidBSTHelper(TreeNode* cur, TreeNode* &prev) {
+        if(!cur) return true;
+        
+        bool ret = isValidBSTHelper(cur->left, prev);
+        if(!ret || (prev && prev->val >= cur->val)) return false;
+        prev = cur;
+        return isValidBSTHelper(cur->right, prev);
+    }
+    
+    bool isValidBST(TreeNode* root) {
+        TreeNode * prev = NULL;
+        return isValidBSTHelper(root, prev);
+    }
+};
+
 int main() {
 
 }
