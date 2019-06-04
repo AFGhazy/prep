a53a53681bf7ad92ae88a192ee508408652924b8
Ghazy <ahmedfathyghazy@gmail.com>
2019-06-04 23:40:45 +0200

    amazon https://leetcode.com/problems/maximum-frequency-stack/

--- design.cpp
+++ design.cpp
@@ -329,6 +329,37 @@ public:
  * vector<string> param_1 = obj->input(c);
  */
 
+class FreqStack {
+public:
+    priority_queue<tuple<int,int,int>> s;
+    map<int,int> mp;
+    int ts;
+    
+    
+    FreqStack() {
+        ts = 0;
+    }
+    
+    void push(int x) {
+        mp[x]++;
+        s.push({mp[x], ts++, x});
+    }
+    
+    int pop() {
+        int val = get<2>(s.top());
+        s.pop();
+        mp[val]--;
+        return val;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ */
+
 int main() {
     
 }
