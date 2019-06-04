faa45ca419152ace4121a1f88b402c54ed491d76
Ghazy <ahmedfathyghazy@gmail.com>
2019-06-04 05:59:49 +0200

    amazon https://leetcode.com/problems/merge-intervals/

--- sorting_searching.cpp
+++ sorting_searching.cpp
@@ -58,6 +58,29 @@ public:
     }
 };
 
+class SolutionMergeIntervals {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int > > v;
+        sort(intervals.begin(), intervals.end());
+        
+        for(auto i: intervals) {
+            int st = i[0], en = i[1];
+            
+            if(v.size() && st <= v.back()[1]) {
+                st = v.back()[0];
+                en = max(i[1], v.back()[1]);
+
+                v.pop_back();
+            }
+            
+            v.push_back({st, en});
+        }
+        
+        return v;
+    }
+};
+
 int main() {
-    
+
 }
