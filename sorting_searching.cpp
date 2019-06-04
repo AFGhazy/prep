da4639f75b9c64549baafb0556db02e9364abf2d
Ghazy <ahmedfathyghazy@gmail.com>
2019-06-04 09:06:34 +0200

    normal partition method

--- sorting_searching.cpp
+++ sorting_searching.cpp
@@ -199,6 +199,28 @@ public:
     }
 };
 
+// normal partition method
+// int partition(int arr[], int begin, int end) {
+//     int pivot = arr[end];
+//     int i = (begin-1);
+ 
+//     for (int j = begin; j < end; j++) {
+//         if (arr[j] <= pivot) {
+//             i++;
+ 
+//             int swapTemp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = swapTemp;
+//         }
+//     }
+ 
+//     int swapTemp = arr[i+1];
+//     arr[i+1] = arr[end];
+//     arr[end] = swapTemp;
+ 
+//     return i+1;
+// }
+
 template<class T>
 void quickSort(vector<T> & v, int l, int r) {
     if(l >= r) return;
